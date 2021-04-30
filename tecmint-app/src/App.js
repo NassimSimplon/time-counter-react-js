e8a69f064e70397433af93efa1622e5e57ce0fca
NassimSimplon <nassimkhlifisimplonenda@gmail.com>
2021-04-30 10:16:19 +0100

    first commit

---
+++ tecmint-app/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Nass from './component/counter.js'
+function App() {
+  return (
+    <div className="App">
+      
+      <Nass />
+    </div>
+    
+  );
+}
+
+export default App;
+document.getElementById("far")
